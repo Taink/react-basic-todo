95c2d46f106c0be322e2429853279e57444fca44
Taink <minemax579@gmail.com>
2021-05-07 14:58:31 +0200

    Add redux

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import { Provider } from 'react-redux'
+import store from './src/redux/store'
+
 import App from './src/App.js'
 
 ReactDOM.render(
-    <App />,
+    <Provider store={store}>
+        <App />
+    </Provider>,
     document.getElementById('root')
 )
