1a57703e63370801f848f64458495076e7cb1587
Taink <minemax579@gmail.com>
2021-05-07 14:45:28 +0200

    Add basic project structure

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import App from './src/App.js'
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+)
